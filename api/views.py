c98b7809d0da01ce622e8a38bbd99d9311c462e2
ZeroExistence <tonydeguia009@gmail.com>
2020-12-11 14:32:46 +0800

    Remove pagination. Move it on the client side.

--- api/views.py
+++ api/views.py
@@ -1,33 +1,16 @@
-from rest_framework import viewsets, pagination
+from rest_framework import viewsets
 from rest_framework.response import Response
-from collections import OrderedDict
-from django.shortcuts import get_list_or_404, get_object_or_404
-from rest_framework.decorators import api_view
+from django.shortcuts import get_list_or_404
 from .models import Post, Tag
 from .serializers import PostSerializer, TagSerializer
 
 # Create your views here.
-
-class PostPagination(pagination.PageNumberPagination):
-    page_size = 12
-    page_size_query_param = 'page_size'
-    max_page_size = 24
-
-    def get_paginated_response(self, data):
-        return Response(OrderedDict([
-            ('current', self.page.number),
-            ('next', self.get_next_link()),
-            ('previous', self.get_previous_link()),
-            ('last_page', self.page.paginator.num_pages),
-            ('results', data)
-        ]))
 
 
 class PostViewSet(viewsets.ReadOnlyModelViewSet):
     lookup_field = 'slug'
     queryset = Post.on_site.all()
     serializer_class = PostSerializer
-    pagination_class = PostPagination
 
 
 class TaggedPostViewSet(viewsets.ReadOnlyModelViewSet):
