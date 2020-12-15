0e39e8b782234cf310f206a62b0584e9a855dc05
ZeroExistence <tonydeguia009@gmail.com>
2020-12-15 15:05:44 +0800

    Add in_sites view in some list pages.

--- api/admin.py
+++ api/admin.py
@@ -4,7 +4,18 @@ from .models import User, Post, Tag, Image
 
 # Register your models here.
 
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_filter = ('sites',)
+    list_display = ('title', 'in_sites',)
+
+
+class TagModelAdmin(admin.ModelAdmin):
+    list_filter = ('sites',)
+    list_display = ('name', 'in_sites')
+
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Post)
-admin.site.register(Tag)
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Tag, TagModelAdmin)
 admin.site.register(Image)
